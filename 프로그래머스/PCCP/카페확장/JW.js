9cd7687b9ec1d1db0ad5e499ac9dadac5d3dfaf5
Jaewoong Hwang <noa9925@gmail.com>
2024-01-31 23:43:38 +0900

    [재웅] 문제 풀이 완료 (1/31, 카페확장, 99분)

--- 프로그래머스/PCCP/카페확장/JW.js
+++ 프로그래머스/PCCP/카페확장/JW.js
@@ -3,7 +3,7 @@ function solution(menu, order, k) {
   let maxResult = 0;
 
   const mappedOrder = order.map((o, i) => {
-    coffeeMadeTime += menu[o];
+    coffeeMadeTime = Math.max(coffeeMadeTime, k * i) + menu[o];
     return [k * i, coffeeMadeTime];
   });
 
@@ -25,16 +25,9 @@ function solution(menu, order, k) {
         stack.push(m);
       }
     });
+
     maxResult = Math.max(maxResult, stack.length);
   });
 
   return maxResult;
-
-  //     1차원으로 spread된 mappedOrder을 순회하며, 해당 시간내 들어올 수 있는 손님을 전부 넣음
-  //     하나씩 빼면서 [0] <= time <= [1]이고,
-  //     queue의 마지막 원소의 퇴장 시간과 현재 시간, 새로 들어오는 원소의 입장 시간이 같을 경우
-  //     queue의 마지막 손님을 내보냄
-  //     해당 시간내에서 mappedOrder을 전부 순회하였을 때 qeueue의 길이를 반환
 }
-
-// 테스트케이스 4, 5, 7, 8, 10 실패
